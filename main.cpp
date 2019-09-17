deb2381df200ca3d03cd3a51de2cc518b175b161
stein <steicb18@wfu.edu>
2019-09-17 15:01:00 -0400

    commit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include<cassert>
 using namespace std;
+int* num1 = nullptr;
 
 // print the values of two variables
 void print_values(int num1, int num2) {
@@ -9,18 +10,16 @@ void print_values(int num1, int num2) {
     cout << "Value of num2 is: " << num2 << endl;
 }
 // swap values of two variables
-void swap(int& num1, int& num2) {
+void swap(int* num1, int* num2) {
     cout<<"Swap function"<<endl;
-    int temp = num1; // save the value of num1
-    num1 = num2; // copy over num2 to num1
-    num2 = temp; // copy the saved value of num1
+    int temp = *num1; // save the value of num1
+    *num1 = *num2; // copy over num2 to num1
+    *num2 = temp; // copy the saved value of num1
 }
 
 int main() {
-
     int num1 = 10;
     int num2 = 20;
-    int temp=num1;
     cout << "Before Swapping: " << endl;
     print_values(num1, num2); // function call
     swap(num1, num2); // swap values?
